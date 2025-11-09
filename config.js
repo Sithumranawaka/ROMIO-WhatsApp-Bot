d031f444b7ec52376172efbe9186a609924d9e50
Sithumranawaka <158823933+Sithumranawaka@users.noreply.github.com>
2025-11-08 21:45:18 -0800

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Sithumranawaka/ROMIO-WhatsApp-Bot/blob/main/images/Gemini_Generated_Image_kzywxnkzywxnkzyw.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 ROMIO-MD Is Alive Now😍*",
 BOT_OWNER: '94776121326',  // Replace with the owner's phone number
 
 
