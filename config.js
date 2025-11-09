23538ff5c6ceb1fae59cfc3825cd34afdfd5a337
Sithumranawaka <158823933+Sithumranawaka@users.noreply.github.com>
2025-11-09 02:52:45 -0800

    Set default SESSION_ID in config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "fE5B0bxa#D7DrMHoUxwA3nyIM2VF2kP1-fUgVp7iHc4Z_ZjORPOo",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Sithumranawaka/ROMIO-WhatsApp-Bot/blob/main/images/Gemini_Generated_Image_kzywxnkzywxnkzyw.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 ROMIO-MD Is Alive Now😍*",
 BOT_OWNER: '94776121326',  // Replace with the owner's phone number
